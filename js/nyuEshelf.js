52d2e78376bf890df70fa23fad4f8f93ed94d9d8
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-14 16:48:30 -0400

    Customize for BHS view

--- js/nyuEshelf.js
+++ js/nyuEshelf.js
@@ -1,6 +1,9 @@
 export let nyuEshelfConfig = {
   name: 'nyuEshelfConfig',
   config: {
-    myEshelfButtonClasses: 'button-over-dark search-bookmark-filter-item'
+    myEshelfButtonClasses: 'button-over-dark search-bookmark-filter-item',
+    guestEshelf: 'Temporary e-Shelf',
+    inGuestEshelf: 'In temporary e-Shelf',
+    loginToSave: ''
   }
 };
