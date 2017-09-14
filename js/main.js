ad14706ad9b31a028dbcaf1cd71ccd5775ff66bf
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-14 16:48:30 -0400

    Customize for BHS view

--- js/main.js
+++ js/main.js
@@ -1,40 +1,23 @@
 import 'primo-explore-custom-actions';
-import 'primo-explore-custom-library-card-menu';
-import 'primo-explore-clickable-logo-to-any-link';
+import 'primo-explore-clickable-logo';
 import 'primo-explore-custom-no-search-results';
-import 'primo-explore-libraryh3lp-widget';
-import 'primo-explore-getit-to-link-resolver';
 import 'primo-explore-nyu-eshelf';
 
 import { viewName } from './viewName';
 import { customActionsConfig } from './customActions';
-import { customLibraryCardMenuItemsConfig } from './customLibraryCardMenu';
-import { clickableLogoLinkConfig } from './clickableLogoToAnyLink';
-import { libraryh3lpWidgetConfig } from './libraryh3lpWidget';
-import { getitToLinkResolverConfig } from './getitToLinkResolver';
 import { nyuEshelfConfig } from './nyuEshelf';
 
 let app = angular.module('viewCustom', [
                                         'customActions',
-                                        'customLibraryCardMenu',
-                                        'clickableLogoToAnyLink',
+                                        'clickableLogo',
                                         'customNoSearchResults',
-                                        'libraryh3lpWidget',
-                                        'getitToLinkResolver',
                                         'nyuEshelf'
                                       ]);
 
 app
   .constant(customActionsConfig.name, customActionsConfig.config)
-  .constant(customLibraryCardMenuItemsConfig.name, customLibraryCardMenuItemsConfig.config)
-  .constant(clickableLogoLinkConfig.name, clickableLogoLinkConfig.config)
-  .constant(libraryh3lpWidgetConfig.name, libraryh3lpWidgetConfig.config)
-  .constant(getitToLinkResolverConfig.name, getitToLinkResolverConfig.config)
   .constant(nyuEshelfConfig.name, nyuEshelfConfig.config)
   .value('customNoSearchResultsTemplateUrl', 'custom/' + viewName + '/html/noSearchResults.html')
-  .component('prmOpacAfter', {
-    template: '<getit-to-link-resolver-full></getit-to-link-resolver-full>'
-  })
   .component('prmSearchResultAvailabilityLineAfter', {
     template: '<nyu-eshelf></nyu-eshelf>'
   })
