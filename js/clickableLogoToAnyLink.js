dff56e161a4cbdbb668c4b7a5784ec1abec4c922
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-20 17:05:12 -0400

    Point BHS logo to homepage

---
+++ js/clickableLogoToAnyLink.js
@@ -0,0 +1,7 @@
+export let clickableLogoLinkConfig = {
+  name: 'clickableLogoLinkConfig',
+  config: {
+    url: 'http://www.brooklynhistory.org/',
+    altText: 'Brooklyn Historical Society'
+  }
+};
--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 import 'primo-explore-custom-actions';
-import 'primo-explore-clickable-logo';
+import 'primo-explore-clickable-logo-to-any-link';
 import 'primo-explore-custom-no-search-results';
 import 'primo-explore-nyu-eshelf';
 
 import { viewName } from './viewName';
 import { customActionsConfig } from './customActions';
+import { clickableLogoLinkConfig } from './clickableLogoToAnyLink';
 import { nyuEshelfConfig } from './nyuEshelf';
 
 let app = angular.module('viewCustom', [
                                         'customActions',
-                                        'clickableLogo',
+                                        'clickableLogoToAnyLink',
                                         'customNoSearchResults',
                                         'nyuEshelf'
                                       ]);
@@ -17,6 +18,7 @@ let app = angular.module('viewCustom', [
 app
   .constant(customActionsConfig.name, customActionsConfig.config)
   .constant(nyuEshelfConfig.name, nyuEshelfConfig.config)
+  .constant(clickableLogoLinkConfig.name, clickableLogoLinkConfig.config)
   .value('customNoSearchResultsTemplateUrl', 'custom/' + viewName + '/html/noSearchResults.html')
   .component('prmSearchResultAvailabilityLineAfter', {
     template: '<nyu-eshelf></nyu-eshelf>'
