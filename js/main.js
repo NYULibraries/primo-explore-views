7cbae5d0d0ff4e1e75cbf96d954914861ecb31a5
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-26 16:18:05 -0400

    Add CU relevant customizations; [Finishes #150865693]

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ import 'primo-explore-custom-actions';
 import 'primo-explore-custom-library-card-menu';
 import 'primo-explore-clickable-logo-to-any-link';
 import 'primo-explore-custom-no-search-results';
-import 'primo-explore-libraryh3lp-widget';
 import 'primo-explore-getit-to-link-resolver';
 import 'primo-explore-nyu-eshelf';
 
@@ -10,7 +9,6 @@ import { viewName } from './viewName';
 import { customActionsConfig } from './customActions';
 import { customLibraryCardMenuItemsConfig } from './customLibraryCardMenu';
 import { clickableLogoLinkConfig } from './clickableLogoToAnyLink';
-import { libraryh3lpWidgetConfig } from './libraryh3lpWidget';
 import { getitToLinkResolverConfig } from './getitToLinkResolver';
 import { nyuEshelfConfig } from './nyuEshelf';
 
@@ -19,7 +17,6 @@ let app = angular.module('viewCustom', [
                                         'customLibraryCardMenu',
                                         'clickableLogoToAnyLink',
                                         'customNoSearchResults',
-                                        'libraryh3lpWidget',
                                         'getitToLinkResolver',
                                         'nyuEshelf'
                                       ]);
@@ -28,7 +25,6 @@ app
   .constant(customActionsConfig.name, customActionsConfig.config)
   .constant(customLibraryCardMenuItemsConfig.name, customLibraryCardMenuItemsConfig.config)
   .constant(clickableLogoLinkConfig.name, clickableLogoLinkConfig.config)
-  .constant(libraryh3lpWidgetConfig.name, libraryh3lpWidgetConfig.config)
   .constant(getitToLinkResolverConfig.name, getitToLinkResolverConfig.config)
   .constant(nyuEshelfConfig.name, nyuEshelfConfig.config)
   .value('customNoSearchResultsTemplateUrl', 'custom/' + viewName + '/html/noSearchResults.html')
