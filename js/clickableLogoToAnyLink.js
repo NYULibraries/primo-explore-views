1b2fa075727ad045f8527fd812fc4fe39fde55e4
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-27 11:26:55 -0400

    Change logo link to main website not new searfch

---
+++ js/clickableLogoToAnyLink.js
@@ -0,0 +1,7 @@
+export let clickableLogoLinkConfig = {
+  name: 'clickableLogoLinkConfig',
+  config: {
+    url: 'http://www.nyhistory.org/library',
+    altText: 'Patricia D. Klingenstein Library'
+  }
+};
--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 import 'primo-explore-custom-actions';
-import 'primo-explore-clickable-logo';
+import 'primo-explore-clickable-logo-to-any-link';
 import 'primo-explore-custom-no-search-results';
 import 'primo-explore-custom-search-bookmark-filter';
 
 import { viewName } from './viewName';
 import { customActionsConfig } from './customActions';
 import { customSearchBookmarkFilterConfig } from './customSearchBookmarkFilter';
+import { clickableLogoLinkConfig } from './clickableLogoToAnyLink';
 
 let app = angular.module('viewCustom', [
                                         'customActions',
-                                        'clickableLogo',
+                                        'clickableLogoToAnyLink',
                                         'customNoSearchResults',
                                         'customSearchBookmarkFilter'
                                       ]);
 
 app
   .constant(customActionsConfig.name, customActionsConfig.config)
+  .constant(clickableLogoLinkConfig.name, clickableLogoLinkConfig.config)
   .constant(customSearchBookmarkFilterConfig.name, customSearchBookmarkFilterConfig.config)
   .component('prmSearchBookmarkFilterAfter', {
     template: '<custom-search-bookmark-filter></custom-search-bookmark-filter>'
