8703f2f05b05aa0c9f437ef11ecefeb17a958874
BAlter <barnaby.alter@nyu.edu>
2018-02-22 12:06:58 -0500

    Added searchBarSubMenu module

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 import 'primo-explore-custom-actions';
 import 'primo-explore-clickable-logo-to-any-link';
 import 'primo-explore-custom-search-bookmark-filter';
+import 'primo-explore-search-bar-sub-menu';
 
 import { viewName } from './viewName';
 import { customActionsConfig } from './customActions';
 import { customSearchBookmarkFilterConfig } from './customSearchBookmarkFilter';
 import { clickableLogoLinkConfig } from './clickableLogoToAnyLink';
+import { searchBarSubMenuItemsConfig } from './searchBarSubMenu';
 
 let app = angular.module('viewCustom', [
                                         'angularLoad',
                                         'customActions',
                                         'clickableLogoToAnyLink',
-                                        'customSearchBookmarkFilter'
+                                        'customSearchBookmarkFilter',
+                                        'searchBarSubMenu'
                                       ]);
 
 app
   .constant(customActionsConfig.name, customActionsConfig.config)
   .constant(clickableLogoLinkConfig.name, clickableLogoLinkConfig.config)
   .constant(customSearchBookmarkFilterConfig.name, customSearchBookmarkFilterConfig.config)
+  .constant(searchBarSubMenuItemsConfig.name, searchBarSubMenuItemsConfig.config)
   .component('prmSearchBookmarkFilterAfter', {
     template: '<custom-search-bookmark-filter></custom-search-bookmark-filter>'
+  })
+  .component('prmSearchBarAfter', {
+    template: '<search-bar-sub-menu></search-bar-sub-menu>'
   })
