c2a28aaafa1bc01a87ddfb58c93506ae809bc74b
BAlter <barnaby.alter@nyu.edu>
2018-02-23 16:40:04 -0500

    Prepare eshelf plugin for production

--- js/nyuEshelf.js
+++ js/nyuEshelf.js
@@ -2,6 +2,13 @@ export let nyuEshelfConfig = {
   name: 'nyuEshelfConfig',
   config: {
     myEshelfButtonClasses: 'button-over-dark search-bookmark-filter-item',
-    institution: 'NYUSH'
+    institution: 'NYUSH',
+    pdsUrl: {
+      base: 'https://pds.library.nyu.edu/pds',
+      callingSystem: 'primo',
+      institution: 'NYUSH-NUI'
+    },
+    bobcatBaseUrl: 'http://bobcat.library.nyu.edu:80',
+    eshelfBaseUrl: 'https://eshelf.library.nyu.edu'
   }
 };
