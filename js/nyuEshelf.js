30e1a7da6f9bc0ec85eaa1dba843b370bae36eb9
BAlter <barnaby.alter@nyu.edu>
2018-03-02 13:52:39 -0500

    Updated e-shelf

--- js/nyuEshelf.js
+++ js/nyuEshelf.js
@@ -2,13 +2,11 @@ export let nyuEshelfConfig = {
   name: 'nyuEshelfConfig',
   config: {
     myEshelfButtonClasses: 'button-over-dark search-bookmark-filter-item',
-    institution: "CU",
-    pdsUrl: {
-      base: 'https://pds.library.nyu.edu/pds',
-      callingSystem: 'primo',
-      institution: 'CU-NUI'
+    defaultUrls: {
+      institution: "CU"
     },
-    bobcatBaseUrl: 'http://bobcat.library.nyu.edu:80',
-    eshelfBaseUrl: 'https://eshelf.library.nyu.edu'
+    "bobcat.library.nyu.edu": {
+      institution: "CU"
+    }
   }
 };
