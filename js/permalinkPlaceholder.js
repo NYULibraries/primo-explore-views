34b34f5e3fc36ad47a744d0cddf9771ceeb6c677
BAlter <barnaby.alter@nyu.edu>
2018-03-02 13:51:57 -0500

    Change layout of permalink placeholder

--- js/permalinkPlaceholder.js
+++ js/permalinkPlaceholder.js
@@ -11,15 +11,15 @@ export let permalinkPlaceholder = angular
               <div class="send-actions-content-item layout-column" layout="column" layout-align="center center">
                 <md-content class="layout-fill layout-padding layout-wrap _md md-primoExplore-theme">
                   <div class="container">
-                    <div class="row">
-                      <div class="col-3">
+                    <md-content layout="row">
+                      <div flex="10" layout="column" layout-align="center center">
                         <prm-icon style="z-index:1" icon-type="svg" svg-icon-set="alert" icon-definition="ic_warning_24px"></prm-icon>
                       </div>
-                      <div class="col-9">
+                      <div flex="90" layout="column">
                         <p>The Permalinks feature isn’t available in BobCat Beta, but will be when we release the new BobCat.</p>
-                        <p>Use <a ng-href="http://bobcat.library.nyu.edu/primo_library/libweb/action/search.do?vid={{ viewName }}">BobCat Classic</a> for persistent links to catalog items. </p>
+                        <p>Use  <a ng-href="http://bobcat.library.nyu.edu/primo_library/libweb/action/search.do?vid={{ viewName }}">BobCat Classic</a> for persistent links to catalog items. </p>
                       </div>
-                    </div>
+                    </md-content>
                   </div>
                 </md-content>
               </div>
