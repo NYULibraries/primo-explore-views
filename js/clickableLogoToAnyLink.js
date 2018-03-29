04454a77cab668059986536cefa467c20eefb902
Elliott Grieco <elliott.grieco@gmail.com>
2018-03-29 17:17:15 -0400

    Removes clickableLogo config

--- js/clickableLogoToAnyLink.js
+++
@@ -1,7 +0,0 @@
-export let clickableLogoLinkConfig = {
-  name: 'clickableLogoLinkConfig',
-  config: {
-    url: 'http://www.brooklynhistory.org/',
-    altText: 'Brooklyn Historical Society'
-  }
-};
