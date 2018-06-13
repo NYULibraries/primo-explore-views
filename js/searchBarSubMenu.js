404a282591b14384325a88eab73f75d1116faf86
BAlter <barnaby.alter@nyu.edu>
2018-06-13 16:16:45 -0400

    Remove return to feedback, added hours and added links to homepage

--- js/searchBarSubMenu.js
+++ js/searchBarSubMenu.js
@@ -2,22 +2,21 @@ export let searchBarSubMenuItemsConfig = {
   name: 'searchBarSubMenuItems',
   config: [
     {
-      name: "Back to BobCat Classic",
-      description: "Back to BobCat Classic",
-      action: "http://bobcat.library.nyu.edu/primo_library/libweb/action/search.do?vid=" + "NYUSH-BKUP",
-      icon: {
-        set: 'navigation',
-        icon: 'ic_arrow_back_24px'
-      },
-      show_xs: true
-    },
-    {
       name: "Provide Feedback",
       description: "Provide Feedback",
       action: "https://nyu.qualtrics.com/jfe/form/SV_blQ3OFOew9vl6Pb?Source=NYUSH",
       icon: {
         set: 'communication',
         icon: 'ic_forum_24px'
+      }
+    },
+    {
+      name: "Library Hours",
+      description: "Library Hours",
+      action: "https://shanghai.nyu.edu/page/library-hours-0",
+      icon: {
+        set: 'av',
+        icon: 'ic_av_timer_24px'
       }
     }
   ]
