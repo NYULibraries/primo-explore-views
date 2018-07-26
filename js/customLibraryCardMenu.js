cac1c4fad8010f69027e30dfa62619c9aaacefe8
Elliott Grieco <elliott.grieco@nyu.edu>
2018-07-26 14:44:07 -0400

    Uses production eshelf

--- js/customLibraryCardMenu.js
+++ js/customLibraryCardMenu.js
@@ -4,7 +4,7 @@ export let customLibraryCardMenuItemsConfig = {
     {
       name: "{nui.menu.librarycard}",
       description: "Go to {nui.menu.librarycard}",
-      action: "https://qa.eshelf.library.nyu.edu/login?institution=NYSID;return_url=https://qa.eshelf.library.nyu.edu/account?institution=NYSID",
+      action: "{urls.eshelf}/login?institution=NYSID;return_url={urls.eshelf}/account?institution=NYSID",
       icon: {
         set: 'social',
         icon: 'ic_person_outline_24px'
