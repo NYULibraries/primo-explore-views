c18505f49ef4d15aced1291b9473002d2befa419
BAlter <barnaby.alter@nyu.edu>
2018-07-27 15:48:08 -0400

    Point to correct account url

--- js/customLibraryCardMenu.js
+++ js/customLibraryCardMenu.js
@@ -4,7 +4,7 @@ export let customLibraryCardMenuItemsConfig = {
     {
       name: "{nui.menu.librarycard}",
       description: "Go to {nui.menu.librarycard}",
-      action: "{urls.eshelf}/login?institution=NYSID;return_url={urls.eshelf}/account?institution=NYSID",
+      action: "{urls.eshelf}/account?institution=NYSID",
       icon: {
         set: 'social',
         icon: 'ic_person_outline_24px'
