3fbbed36b7de36713820f5c5f0e84f062ddc1034
BAlter <barnaby.alter@nyu.edu>
2018-08-06 10:54:54 -0400

    Remove permalinkPlaceholder

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 import 'primo-explore-custom-no-search-results';
 
-import { permalinkPlaceholder } from './permalinkPlaceholder';
-
 let app = angular.module('centralCustom', [
                                         'angularLoad',
-                                        'customNoSearchResults',
-                                        'permalinkPlaceholder'
+                                        'customNoSearchResults'
                                       ]);
 
 app
   .value('customNoSearchResultsTemplateUrl', 'custom/CENTRAL_PACKAGE/html/noSearchResults.html')
-  .component('prmPermalinkAfter', {
-    template: '<permalink-placeholder></permalink-placeholder>'
-  })
