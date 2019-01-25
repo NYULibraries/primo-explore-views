fa31151f9b60c4a685107d3c27732301914310cf
Elliott Grieco <elliott.grieco@nyu.edu>
2019-01-25 13:19:25 -0500

    Updates custom-login and custom-requests and corresponding configs

--- js/customLoginConfig.js
+++ js/customLoginConfig.js
@@ -1,3 +1,7 @@
+const mockUserWarning = () => console.warn(`This build is using a mock user!` +
+  ` Use NODE_ENV that is not 'development' (e.g. 'staging')` +
+  ` to disable the mock user feature`);
+
 export default {
   name: 'primoExploreCustomLoginConfig',
   config: {
@@ -5,7 +9,7 @@ export default {
     queryString: 'func=get-attribute&attribute=bor_info',
     selectors: ['id', 'bor-status'],
     mockUserConfig: {
-      // enabled: true,
+      enabled: process.env.NODE_ENV === 'development' ? (mockUserWarning(), true) : false,
       user: {
         'id': '1234567',
         'bor-status': '50',
