7fd5534da34638724758bddbe4feea763ef21bf3
BAlter <barnaby.alter@nyu.edu>
2019-01-25 11:03:59 -0500

    Updated custom login to use mock user

--- js/customLoginConfig.js
+++ js/customLoginConfig.js
@@ -1,8 +1,16 @@
 export default {
-    name: 'primoExploreCustomLoginConfig',
-    config: {
-      pdsUrl: `https://pds${process.env.NODE_ENV !== 'production' ? 'dev' : ''}.library.nyu.edu/pds`,
-      queryString: 'func=get-attribute&attribute=bor_info',
-      selectors: ['id', 'bor-status'],
-    },
-  };
+  name: 'primoExploreCustomLoginConfig',
+  config: {
+    pdsUrl: `https://pds${process.env.NODE_ENV !== 'production' ? 'dev' : ''}.library.nyu.edu/pds`,
+    queryString: 'func=get-attribute&attribute=bor_info',
+    selectors: ['id', 'bor-status'],
+    // mockUserConfig: {
+    //   enabled: true,
+    //   user: {
+    //     'id': '1234567',
+    //     'bor-status': '20',
+    //   },
+    //   delay: 500,
+    // }
+  },
+};
