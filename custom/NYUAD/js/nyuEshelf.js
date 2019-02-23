91ffa9401a20cf098f38525f07f0b81fe011dee1
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-22 20:14:35 -0500

    Adds NYUAD files

---
+++ custom/NYUAD/js/nyuEshelf.js
@@ -0,0 +1,12 @@
+export let nyuEshelfConfig = {
+  name: 'nyuEshelfConfig',
+  config: {
+    myEshelfButtonClasses: 'button-over-dark search-bookmark-filter-item',
+    defaultUrls: {
+      institution: "NYUAD"
+    },
+    "bobcat.library.nyu.edu": {
+      institution: "NYUAD"
+    }
+  }
+};
---
+++ custom/NYUAD/js/searchBarSubMenu.js
@@ -0,0 +1,23 @@
+export let searchBarSubMenuItemsConfig = {
+  name: 'searchBarSubMenuItems',
+  config: [
+    {
+      name: "Provide Feedback",
+      description: "Provide Feedback",
+      action: "https://nyu.qualtrics.com/jfe/form/SV_blQ3OFOew9vl6Pb?Source=NYUAD",
+      icon: {
+        set: 'communication',
+        icon: 'ic_forum_24px'
+      }
+    },
+    {
+      name: "Library Hours",
+      description: "Library Hours",
+      action: "https://nyuad.nyu.edu/en/library/using-the-library/contact/opening-hours.html",
+      icon: {
+        set: 'av',
+        icon: 'ic_av_timer_24px'
+      }
+    }
+  ]
+};
