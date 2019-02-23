e6bef01ba934e3580c4f1e117f0ceb2993f9ce07
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-22 19:18:01 -0500

    Avoids non-zero exits

--- script/create_package.sh
+++ script/create_package.sh
@@ -3,8 +3,8 @@
 VIEWS='NYU NYUSH NYUAD NYSID BHS NYHS HSL'
 for VIEW in $VIEWS
 do
-  git diff-tree --no-commit-id --name-only -r HEAD | grep /${VIEW}/
-  if [[ $? == 0 ]]; then
+  MATCHES=$(git diff-tree --no-commit-id --name-only -r HEAD | grep -c /${VIEW}/)
+  if [[ $MATCHES != 0 ]]; then
     export NODE_ENV=$([[ $CIRCLE_BRANCH = master ]] && echo "production" || echo "staging")
     VIEW=$VIEW docker-compose run create-package
     docker cp "$(docker ps -q -a -l -f name=create-package)":/app/packages packages
--- script/run_tests.sh
+++ script/run_tests.sh
@@ -3,8 +3,8 @@
 VIEWS='NYU NYUSH NYUAD NYSID BHS NYHS HSL'
 for VIEW in $VIEWS
 do
-  git diff-tree --no-commit-id --name-only -r HEAD | grep /${VIEW}/
-  if [[ $? == 0 ]]; then
+  MATCHES=$(git diff-tree --no-commit-id --name-only -r HEAD | grep -c /${VIEW}/)
+  if [[ $MATCHES != 0 ]]; then
     echo "Files changed in $VIEW package. Running tests."
     VIEW=$VIEW docker-compose run e2e cypress run --spec "cypress/integration/${VIEW}/**/*.spec.js" --reporter junit --reporter-options "mochaFile=test-results/${VIEW}/results-[hash].xml"
     docker cp "$(docker ps -q -a -l -f name=e2e)":/app/cypress cypress-results
