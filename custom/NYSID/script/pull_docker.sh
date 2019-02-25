f9608dd234c64cb82fa1e468ed01a83eac1d7ac6
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-25 13:04:33 -0500

    Reyarns, moves e2e, removes redundant CI files

--- custom/NYSID/script/pull_docker.sh
+++
@@ -1,3 +0,0 @@
-# #!/bin/sh -ex
-
-docker pull quay.io/nyulibraries/primo-explore-nysid:${CIRCLE_BRANCH//\//_} || docker pull quay.io/nyulibraries/primo-explore-nysid:latest
--- custom/NYSID/script/push_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ex
-
-docker tag primo-explore-nysid quay.io/nyulibraries/primo-explore-nysid:latest
-docker tag primo-explore-nysid quay.io/nyulibraries/primo-explore-nysid:${CIRCLE_BRANCH//\//_}
-docker tag primo-explore-nysid quay.io/nyulibraries/primo-explore-nysid:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
-
-docker push quay.io/nyulibraries/primo-explore-nysid:latest
-docker push quay.io/nyulibraries/primo-explore-nysid:${CIRCLE_BRANCH//\//_}
-docker push quay.io/nyulibraries/primo-explore-nysid:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
