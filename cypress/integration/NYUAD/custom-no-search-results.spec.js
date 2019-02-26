38b252084daed4cdfba63e9fa74115bf330dd301
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-26 14:52:48 -0500

    Improves test coverage custom-no-search NYUSH, NYUAD

--- cypress/integration/NYUAD/custom-no-search-results.spec.js
+++ cypress/integration/NYUAD/custom-no-search-results.spec.js
@@ -2,18 +2,20 @@ describe('primo-explore-custom-no-search-results', function () {
   describe('when a search returns no results', () => {
     // bad search:asf;afsd;/1!
     before(() => {
-      cy.visit(`?query=any,contains,asf;afsd;~2F1!&tab=all&search_scope=all&vid=NYUSH&offset=0`)
+      cy.visit(`?query=any,contains,asf;afsd;~2F1!&tab=all&search_scope=all&vid=NYUAD&offset=0`)
     })
 
     it('renders No records found', () => {
       cy.get(`[data-cy=no-search-results]`)
         .contains(`No records found`)
+        .should('exist')
     })
 
     describe('its links', () => {
       const links = {
+        ['Request a book from E-ZBorrow (NYU only)']: `https://login.library.nyu.edu/ezborrow/nyuad?query=asf%3Bafsd%3B~2F1!`,
         [`Search WorldCat for items in nearby libraries`]: `http://www.worldcat.org/search?qt=worldcat_org_all&q=asf%3Bafsd%3B~2F1!`,
-        [`Ask a Librarian`]: `https://shanghai.nyu.edu/academics/library/services/aal`,
+        [`Ask a Librarian`]: `https://nyuad.nyu.edu/en/library/research-and-instruction-services/research-librarians/ask-a-librarian.html`,
       }
 
       it(`includes the expected number of links`, () => {
