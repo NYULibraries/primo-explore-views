5b716e430ef76e96f5313421d5400e70c07b7682
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-26 11:55:42 -0500

    Fixes BHS url

--- cypress/integration/BHS/home.spec.js
+++ cypress/integration/BHS/home.spec.js
@@ -1,6 +1,6 @@
 describe('The Home Page', function () {
   before(() => {
-    cy.visit('?vid=NYUAD') // change URL to match your dev URL
+    cy.visit('?vid=BHS') // change URL to match your dev URL
   })
 
   it('successfully loads', function () {
