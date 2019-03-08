134d09dddce554cae12dd54cd3d984cbc5f34ac9
Elliott Grieco <elliott.grieco@nyu.edu>
2019-03-08 13:23:57 -0500

    Adds sentry logging for library.nyu.edu url rejections

--- custom/common/js/sentryConfig.js
+++ custom/common/js/sentryConfig.js
@@ -1,7 +1,7 @@
 export default {
   dsn: 'https://7527da50c7da4590ae8dcd1d6b56ee55@sentry.io/1394419',
   whitelistUrls: [
-    /library\.nyu\.edu/,
+    /https?:\/\/bobcat.*\.library\.nyu\.edu/,
   ],
   sanitizeKeys: [
     'pds_handle',
