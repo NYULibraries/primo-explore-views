512ef91a9d65533e07f3a1ff2672fe82a142cc6d
Elliott Grieco <elliott.grieco@nyu.edu>
2019-03-13 17:36:26 -0400

    Adds custom-login files with sample spec file

---
+++ modules/primo-explore-custom-login/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    filename: 'index.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader'
+    }]
+  },
+  devtool: 'sourcemap',
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+};
