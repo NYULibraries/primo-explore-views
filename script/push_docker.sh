52fc149bfab87e2c72adca7ed3b9c3c2ab4f75f5
Elliott Grieco <elliott.grieco@nyu.edu>
2019-03-15 13:46:48 -0400

    Segregates e2e tests more coherently

--- script/push_docker.sh
+++ script/push_docker.sh
@@ -1,9 +1,14 @@
 #!/bin/sh -ex
 
-docker tag primo-explore-views quay.io/nyulibraries/primo-explore-views:latest
-docker tag primo-explore-views quay.io/nyulibraries/primo-explore-views:${CIRCLE_BRANCH//\//_}
-docker tag primo-explore-views quay.io/nyulibraries/primo-explore-views:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+# Executes docker build if primo-explore-views has not been built
+if [[ $(docker image ls --filter="reference=primo-explore-views:latest" --format "{{.ID}}" | grep "" -c) < 1 ]]; then
+  docker-compose build web
+else
+  docker tag primo-explore-views quay.io/nyulibraries/primo-explore-views:latest
+  docker tag primo-explore-views quay.io/nyulibraries/primo-explore-views:${CIRCLE_BRANCH//\//_}
+  docker tag primo-explore-views quay.io/nyulibraries/primo-explore-views:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
-docker push quay.io/nyulibraries/primo-explore-views:latest
-docker push quay.io/nyulibraries/primo-explore-views:${CIRCLE_BRANCH//\//_}
-docker push quay.io/nyulibraries/primo-explore-views:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+  docker push quay.io/nyulibraries/primo-explore-views:latest
+  docker push quay.io/nyulibraries/primo-explore-views:${CIRCLE_BRANCH//\//_}
+  docker push quay.io/nyulibraries/primo-explore-views:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+fi
