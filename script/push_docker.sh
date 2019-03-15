aa78fcae3c5d3d66f0bd2b84b4107039c295d909
Elliott Grieco <elliott.grieco@nyu.edu>
2019-03-15 15:11:51 -0400

    Always rebuilds web before docker push

--- script/push_docker.sh
+++ script/push_docker.sh
@@ -1,9 +1,6 @@
 #!/bin/sh -ex
-
-# Executes docker build if primo-explore-views has not been built
-if [[ $(docker image ls --filter="reference=primo-explore-views:latest" --format "{{.ID}}" | grep "" -c) < 1 ]]; then
   docker-compose build web
-else
+
   docker tag primo-explore-views quay.io/nyulibraries/primo-explore-views:latest
   docker tag primo-explore-views quay.io/nyulibraries/primo-explore-views:${CIRCLE_BRANCH//\//_}
   docker tag primo-explore-views quay.io/nyulibraries/primo-explore-views:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
