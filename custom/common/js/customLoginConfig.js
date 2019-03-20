94deb14ca7d5b65572220ce233c4ccd5ea039cd6
Elliott Grieco <elliott.grieco@nyu.edu>
2019-03-20 12:52:31 -0400

    Adds custom login features, Fixes user loading bug

--- custom/common/js/customLoginConfig.js
+++ custom/common/js/customLoginConfig.js
@@ -5,22 +5,34 @@ const mockUserWarning = () => console.warn(`This build is using a mock user!` +
 export default {
   name: 'primoExploreCustomLoginConfig',
   config: {
-    pdsUrl: `https://pds${process.env.NODE_ENV !== 'production' ? 'dev' : ''}.library.nyu.edu/pds`,
-    queryString: 'func=get-attribute&attribute=bor_info',
-    selectors: ['id', 'bor-status'],
+    pdsUrl: `https://pds${process.env.NODE_ENV !== 'production' ? 'dev' : ''}.library.nyu.edu/pds?func=get-attribute&attribute=bor_info`,
+    callback(response, $window) {
+      const selectors = ['id', 'bor-status'];
+      const xml = response.data;
+      const getXMLProp = prop => (new $window.DOMParser).parseFromString(xml, 'text/xml').querySelector(prop).textContent;
+
+      // getXMLProp for each selector, then merge to object
+      const user = selectors.reduce((res, prop) => ({ ...res, [prop]: getXMLProp(prop) }), {});
+
+      return user;
+    },
     mockUserConfig: {
       get enabled() {
-        return /^(development|test)$/.test(process.env.NODE_ENV) ? mockUserWarning() || true : false;
+        if (/^(development|test)$/.test(process.env.NODE_ENV)) {
+          mockUserWarning();
+          return true;
+        } else {
+          return false;
+        }
       },
       get isLoggedIn() {
-        const devEnvironment = /^(development|test)$/.test(process.env.NODE_ENV);
-        return devEnvironment ? window.$$mockUserLoggedIn : undefined;
+        return /^(development|test)$/.test(process.env.NODE_ENV) ? window.$$mockUserLoggedIn : undefined;
       },
       user: {
         'id': '1234567',
         'bor-status': '50',
       },
-      delay: 500,
+      delay: 3000,
     }
   },
 };
