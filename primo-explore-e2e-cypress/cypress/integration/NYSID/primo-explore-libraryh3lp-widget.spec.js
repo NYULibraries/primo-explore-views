72d72355d6c44f07aa8c9ea8506c53b8b200f3ea
Elliott Grieco <elliott.grieco@nyu.edu>
2019-04-09 16:33:25 -0400

    Chore/upgrade libraryh3lp widget (#45)
    * Uses git branch version of libraryh3lp
    
    * Adds libraryh3lp integration tests
    
    * Uses devenv 1.0.7
    
    * Refines dockerignore, docker-compose

---
+++ primo-explore-e2e-cypress/cypress/integration/NYSID/primo-explore-libraryh3lp-widget.spec.js
@@ -0,0 +1,42 @@
+describe('primo-explore-libraryh3lp-widget', () => {
+  describe('when visiting the homepage', () => {
+    before(() => {
+      cy.visit('/search?vid=NYSID')
+    })
+
+    it('has a visible open chat button', () => {
+      cy.get('button.chat-tab.ss-chat.js-toggle-chat')
+        .should('be.visible')
+    })
+
+    it('does not have a visible libraryh3lp element', () => {
+      cy.get('.libraryh3lp')
+        .should('not.be.visible')
+    })
+
+    it('does not have a visible close button', () => {
+      cy.get('button.close-chat')
+        .should('not.be.visible')
+    })
+
+    describe('when the Chat with us button is clicked', () => {
+      before(() => {
+        cy.contains('Chat with us')
+          .click('center')
+      })
+
+      it('it opens a visible libraryh3lp iframe', () => {
+        cy.get('.libraryh3lp iframe')
+          .should('be.visible')
+      })
+
+      it('can close the iframe when clicking the close button', () => {
+        cy.get('button.chat-close')
+          .should('be.visible')
+          .click('center')
+          .get('.libraryh3lp iframe')
+          .should('not.be.visible')
+      })
+    })
+  })
+})
