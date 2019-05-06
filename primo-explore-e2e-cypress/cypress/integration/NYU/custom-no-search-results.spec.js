dfd5f499c7824c58b7da2b7d49918a07e1a747ee
Elliott Grieco <elliott.grieco@nyu.edu>
2019-05-06 17:00:26 -0400

    Adds citation linker to no results (#76)
    * Adds citation linker to no results
    
    * Adds citation linker link

--- primo-explore-e2e-cypress/cypress/integration/NYU/custom-no-search-results.spec.js
+++ primo-explore-e2e-cypress/cypress/integration/NYU/custom-no-search-results.spec.js
@@ -15,6 +15,7 @@ describe('primo-explore-custom-no-search-results', function () {
       const links = {
         ['Request a book from E-ZBorrow (NYU only)']: `https://login.library.nyu.edu/ezborrow?query=asf%3Bafsd%3B~2F1!`,
         [`Search WorldCat for items in nearby libraries`]: `http://www.worldcat.org/search?qt=worldcat_org_all&q=asf%3Bafsd%3B~2F1!`,
+        [`Have a full citation? Use the Citation Linker`]: `/primo-explore/citationlinker?vid=NYU`,
         [`Ask a Librarian`]: `http://library.nyu.edu/ask`,
       }
 
@@ -23,9 +24,10 @@ describe('primo-explore-custom-no-search-results', function () {
           .should('have.lengthOf', Object.keys(links).length)
       })
 
-      Object.entries(links).forEach(([text, href]) => {
-        it(`includes information about: ${text}`, () => {
-          cy.get(`[data-cy=no-results-more-info]`)
+      Object.entries(links).forEach(([text, href], idx) => {
+        it(`${idx} list item includes information about: ${text}`, () => {
+          cy.get(`[data-cy=no-results-more-info] li`)
+            .eq(idx)
             .contains(text)
             .should('have.attr', 'href', href)
         })
