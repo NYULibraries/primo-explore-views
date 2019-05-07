380ffb1014248decc3a00262347c0f575f6cf3f1
Elliott Grieco <elliott.grieco@nyu.edu>
2019-05-07 16:50:01 -0400

    Feature/citation linker cta (#82)
    * Adds call to action button and custom stylings

--- custom/NYU/js/main.js
+++ custom/NYU/js/main.js
@@ -23,6 +23,7 @@ import customRequestsConfig from 'Common/js/customRequestsConfig';
 import customLoginConfig from 'Common/js/customLoginConfig';
 // Common alias does not work for HTML imports
 import customRequestsRequestInformationTemplate from '../html/custom_requests_request_information.html';
+import citationLinkerAfterTemplate from '../html/citation_linker_after.html';
 
 
 let app = angular.module('viewCustom', [
@@ -131,6 +132,9 @@ app
   })
   .component('prmLocationItemsAfter', {
     template: `${customRequestsRequestInformationTemplate}`
+  })
+  .component('prmCitationLinkerAfter', {
+    template: citationLinkerAfterTemplate,
   });
 
 app.run(runBlock);
