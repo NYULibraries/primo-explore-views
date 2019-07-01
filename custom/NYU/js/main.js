b9a26190fa9e5c7cc6171374194bc7b4ac2db84c
Elliott Grieco <elliott.grieco@nyu.edu>
2019-07-01 13:21:12 -0400

    Refactors getit and GA send to code

--- custom/NYU/js/main.js
+++ custom/NYU/js/main.js
@@ -23,6 +23,7 @@ import customRequestsConfig from 'Common/js/customRequestsConfig';
 import customLoginConfig from 'Common/js/customLoginConfig';
 // Common alias does not work for HTML imports
 import customRequestsRequestInformationTemplate from '../html/custom_requests_request_information.html';
+import getitLegacyNotification from './getitLegacyNotification';
 
 
 let app = angular.module('viewCustom', [
@@ -54,42 +55,53 @@ app
     controller: ['$window', function($window) {
       const ctrl = this;
 
-      ctrl.$onInit= () => {
-        $window.prmActionCtrl = ctrl.prmActionCtrl;
+      // takes a 'key' string and executes a gtag event using the key to categorize it
+      ctrl.gaSendAction = key => {
+        // console.log('sent', key);
+        $window.gtag('event', 'send_to', {
+          event_category: key,
+          event_label: ctrl.prmActionCtrl.isFullView ? 'Full view' : 'Brief view',
+        });
       };
 
-      ctrl.onToggleKeys = [];
-      ctrl.amendedActions = {};
-      ctrl.$doCheck = () => {
-        const currentOnToggleKeys = Object.keys(ctrl.prmActionCtrl.actionListService.onToggle);
-        if (currentOnToggleKeys.some(key => !ctrl.onToggleKeys.includes(key))) {
-          ctrl.onToggleKeys = currentOnToggleKeys;
-
-          const actionFunctions = Object.freeze(Object.assign({}, ctrl.prmActionCtrl.actionListService.onToggle));
-          const analyzedActionFunctions = Object.entries(actionFunctions)
-                                                .map(ctrl.gaSendAction)
-                                                .reduce((res, [key, value]) => ({ ...res, [key]: value }), {});
-
-          Object.assign(ctrl.prmActionCtrl.actionListService.onToggle, analyzedActionFunctions);
+      // utility function for prepending arbitrary, no argument function to the second element (a function) in a [key, fxn] array
+      // takes optional third argument for memoizing functions that have already been appended
+      // mutates the memo only. Does not mutate the original object received
+      ctrl.appendActions = ([key, fxn], appendFxn, amendmentMemo = {}) => {
+        if (amendmentMemo[key]) {
+          return [key, fxn];
+        } else {
+          amendmentMemo[key] = true;
+          // console.log('ammended', key);
+          return [
+            key,
+            (...args) => {
+              appendFxn();
+              fxn(...args);
+            },
+          ];
         }
       };
 
-      ctrl.gaSendAction = ([actionKey, actionFunction]) => {
-        if (ctrl.amendedActions[actionKey]) {
-          return [actionKey, actionFunction];
-        } else {
-          ctrl.amendedActions[actionKey] = true;
+      ctrl.amendedToggleFunctions = {};
+      ctrl.$doCheck = () => {
+        // get the onToggle object, which is POJO of handlers to execute when a
+        // certain send to button is clicked
+        const onToggle = ctrl.prmActionCtrl.actionListService.onToggle;
+        // if any of the current onToggle functions have not been ammended
+        const anyNotAmended = Object.keys(onToggle).some(toggleKey => !ctrl.amendedToggleFunctions[toggleKey]);
+        if (anyNotAmended) {
+          // then make a new dictionary of onToggle amended with a GA step
+          const newlyAmendedActions = Object.entries(onToggle)
+                                            .map((entry) => ctrl.appendActions(
+                                              entry,
+                                              () => ctrl.gaSendAction(entry[0]),
+                                              ctrl.amendedToggleFunctions
+                                            ))
+                                            .reduce((res, [key, value]) => ({ ...res, [key]: value }), {});
 
-          return [
-            actionKey,
-            (...args) => {
-              $window.gtag('event', 'send_to', {
-                event_category: actionKey,
-                event_label: ctrl.prmActionCtrl.isFullView ? 'Full view' : 'Brief view',
-              });
-              actionFunction(...args);
-            },
-          ];
+          // and, finally, overwrite the onToggle functions
+          Object.assign(onToggle, newlyAmendedActions);
         }
       };
     }],
@@ -101,53 +113,7 @@ app
   .component('prmFullViewServiceContainerAfter', {
       template: /*html*/`<getit-to-link-resolver-full></getit-to-link-resolver-full>`
   })
-  // .component('prmFullViewServiceContainerAfter', {
-  //   template: /*html*/`
-  //     <getit-to-link-resolver-full></getit-to-link-resolver-full>
-  //     <div ng-if="$ctrl.isSendTo" class="getit-to-link-resolver-full-container">
-  //       <div class="section-head">
-  //         <div layout="row" layout-align="center center">
-  //           <h4 class="section-title md-title light-text">New Feature Alert!</h4>
-  //             <md-divider flex class="md-primoExplore-theme"></md-divider>
-  //         </div>
-  //       </div>
-  //       <div class="section-body">
-  //         <div
-  //           layout="row"
-  //           layout-align="center center"
-  //           class="bar alert-bar zero-margin-bottom"
-  //         >
-  //           <span class="bar-text margin-right-small">
-  //             Don't see E-journals, E-books, or HathiTrust results, etc.? Use the
-  //             <a href="#getit-full" ng-click="$ctrl.handleAnchor('getit-full', $event)">
-  //               GetIt (Legacy Feature)
-  //               <span class="sr-only">Skip to GetIt Legacy</span>
-  //             </a>
-  //             link below while we work to add those results to this new feature.
-  //           </span>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   `,
-  //   controller: ['$anchorScroll', '$window', function ($anchorScroll, $window) {
-  //     const ctrl = this;
-  //     ctrl.$onInit = function () {
-  //       ctrl.isSendTo = ctrl.parentCtrl.service.title === 'nui.brief.results.tabs.send_to';
-
-  //       ctrl.handleAnchor = (id, $event) => {
-  //         $event.preventDefault();
-  //         // sets yOffsetProperty based on jQuery element height, then resets to default value
-  //         $anchorScroll.yOffset = angular.element($window.document.querySelector(`md-toolbar.default-toolbar`));
-  //         $anchorScroll(id);
-  //         // focuses element
-  //         angular.element($window.document.querySelector(`#${id} a`)).focus();
-  //       };
-  //     };
-  //   }],
-  //   bindings: {
-  //     parentCtrl: '<',
-  //   }
-  // })
+  // .component('prmFullViewServiceContainerAfter', getitLegacyNotification)
   .component('prmSearchResultAvailabilityLineAfter', {
     template: /*html*/`<nyu-eshelf></nyu-eshelf>`
   })
