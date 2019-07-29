00389543ef3afc5949b408f2886111ed4b8e7f94
Elliott Grieco <elliott.grieco@nyu.edu>
2019-07-29 09:32:07 -0400

    Updates testing, some scripts

--- script/run_view_e2e_test.sh
+++ script/run_view_e2e_test.sh
@@ -5,9 +5,9 @@
 
 mkdir -p cypress-results
 
-docker-compose run e2e bash -c 'script/wait_for.sh http://web-test:8004/primo-explore/search && yarn cypress run --spec="cypress/integration/$VIEW/**/*.spec.js" --reporter="junit" --reporter-options="mochaFile=test-results/$VIEW/results-[hash].xml"' || ANY_FAILS=$ANY_FAILS$?
-docker cp "$(docker ps -q -a -l -f name=e2e)":/app/cypress/videos cypress-results/ || : # escape failure if video does not exist
-docker cp "$(docker ps -q -a -l -f name=e2e)":/app/cypress/screenshots cypress-results/ || : # screenshots only on failures
+docker-compose run e2e || ANY_FAILS=$ANY_FAILS$?
+docker cp "$(docker ps -q -a -l -f name=e2e)":/app/cypress/videos cypress-results/ || : # escape failure if video do not exist
+docker cp "$(docker ps -q -a -l -f name=e2e)":/app/cypress/screenshots cypress-results/ || : # escape failure if screenshots do not exist
 docker cp "$(docker ps -q -a -l -f name=e2e)":/app/test-results cypress-results/
 docker-compose down
 # Checks if non-zero exit code occurred
