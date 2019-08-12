445e0402ffb7c422d0d96446c49f5ec3028abc3b
Elliott Grieco <elliott.grieco@nyu.edu>
2019-08-12 15:02:57 -0400

    Upgrades modules to bumped versions

--- modules/docker-entrypoint.sh
+++ modules/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 yarn config set cache-folder $YARN_CACHE_FOLDER
 cd $CUSTOMIZATIONS_PATH
-(yarn check --integrity && yarn check --verify-tree) || yarn install --frozen-lockfile
+(yarn check --integrity && yarn check --verify-tree) || yarn install
 cd $DEVENV_PATH
 
 # Call command issued to the docker service
