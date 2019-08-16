ca43ef5e2cb7b3a8016298381706ed661fb389b3
Elliott Grieco <elliott.grieco@nyu.edu>
2019-08-16 15:05:19 -0400

    Adds junit reporter and stores testing artifacts

--- custom/spec/support/helpers/reporter.js
+++ custom/spec/support/helpers/reporter.js
@@ -1,9 +1,15 @@
-// https://github.com/bcaudan/jasmine-spec-reporter/tree/93e3132168f049f3307965206d407068ea3bbcef/examples/node
 const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const JUnitXmlReporter = require('jasmine-reporters').JUnitXmlReporter;
+
+const path = require('path');
 
 jasmine.getEnv().clearReporters(); // remove default reporter logs
 jasmine.getEnv().addReporter(new SpecReporter({ // add jasmine-spec-reporter
   spec: {
     displayPending: true
   }
+}));
+jasmine.getEnv().addReporter(new JUnitXmlReporter({
+  savePath: path.join(__dirname, '../../junit'),
+  consolidateAll: false
 }));
