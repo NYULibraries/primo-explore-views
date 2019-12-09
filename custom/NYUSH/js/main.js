638e044e90168c99ce55f840425bab807f3894d8
BAlter <barnaby.alter@nyu.edu>
2019-12-09 15:15:45 -0500

    Removed getit-to-link-resolver from NYUSH view

--- custom/NYUSH/js/main.js
+++ custom/NYUSH/js/main.js
@@ -5,7 +5,6 @@ import '@orbis-cascade/primo-explore-custom-actions';
 import 'primo-explore-custom-library-card-menu';
 import 'primo-explore-clickable-logo-to-any-link';
 import 'primo-explore-libraryh3lp-widget';
-import 'primo-explore-getit-to-link-resolver';
 import 'primo-explore-nyu-eshelf';
 import 'primo-explore-search-bar-sub-menu';
 import 'primo-explore-custom-requests';
@@ -16,7 +15,6 @@ import customActionsConfig from './customActions';
 import customLibraryCardMenuItemsConfig from './customLibraryCardMenu';
 import clickableLogoLinkConfig from './clickableLogoToAnyLink';
 import libraryh3lpWidgetConfig from './libraryh3lpWidget';
-import getitToLinkResolverConfig from './getitToLinkResolver';
 import nyuEshelfConfig from './nyuEshelf';
 import searchBarSubMenuItemsConfig from './searchBarSubMenu';
 import customRequestsConfig from 'Common/js/customRequestsConfig';
@@ -32,7 +30,6 @@ let app = angular.module('viewCustom', [
   'customLibraryCardMenu',
   'clickableLogoToAnyLink',
   'libraryh3lpWidget',
-  'getitToLinkResolver',
   'nyuEshelf',
   'searchBarSubMenu',
   'primoExploreCustomLogin',
@@ -43,7 +40,6 @@ app
   .constant(customLibraryCardMenuItemsConfig.name, customLibraryCardMenuItemsConfig.config)
   .constant(clickableLogoLinkConfig.name, clickableLogoLinkConfig.config)
   .constant(libraryh3lpWidgetConfig.name, libraryh3lpWidgetConfig.config)
-  .constant(getitToLinkResolverConfig.name, getitToLinkResolverConfig.config)
   .constant(nyuEshelfConfig.name, nyuEshelfConfig.config)
   .constant(searchBarSubMenuItemsConfig.name, searchBarSubMenuItemsConfig.config)
   .constant(customRequestsConfig.name, customRequestsConfig.config(vid))
@@ -54,9 +50,6 @@ app
   }])
   .component('prmActionListAfter', {
     template: customActionsConfig.template
-  })
-  .component('prmFullViewServiceContainerAfter', {
-    template: /*html*/`<getit-to-link-resolver-full></getit-to-link-resolver-full>`
   })
   .component('prmSearchResultAvailabilityLineAfter', {
     template: /*html*/`<nyu-eshelf></nyu-eshelf>`
