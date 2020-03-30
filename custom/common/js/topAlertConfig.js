358b0144b2d9518a0570438836f24ef687278bba
BAlter <barnaby.alter@nyu.edu>
2020-03-30 16:22:04 -0400

    Setup topAlert to be common across any institution that includes it

---
+++ custom/common/js/topAlertConfig.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'customTopAlertConfig',
+  config: 
+    {
+      alert_text: "{custom.banner_value1}",
+      alert_url: "{custom.banner_value2}",
+      alert_see_more_text: "See more",
+    }
+};
