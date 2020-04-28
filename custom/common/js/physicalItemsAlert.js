fafbd46f6eec5bfc7cdcc0284a5f24f7b07244c6
BAlter <barnaby.alter@nyu.edu>
2020-04-28 09:17:17 -0400

    Tweaks covid alert link formatting

--- custom/common/js/physicalItemsAlert.js
+++ custom/common/js/physicalItemsAlert.js
@@ -6,13 +6,17 @@ export default {
       <div class="bar large-bar layout-fill layout-padding layout-align-center-center layout-row error-bar">
         <span class="bar-text">
           Due to COVID-19, all NYU Libraries locations are closed for more information visit our Status Page URL: 
-          <a href="https://nyulibraries.statuspage.io/" target="_blank">https://nyulibraries.statuspage.io/ 
-            <prm-icon external-link icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new"></prm-icon>
+          <a href="https://nyulibraries.statuspage.io/" target="_blank">https://nyulibraries.statuspage.io
             <span class="sr-only">(opens in a new window)</span>
-          </a>
+            <prm-icon external-link icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new">
+            </prm-icon>
+          </a>.
           Access to physical materials will resume when our locations reopen. At present requests can only be filled digitally. We are exploring 
-          <a href="https://library.nyu.edu/services/remote/" target="_blank">all sourcing options <prm-icon external-link icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new"></prm-icon>
-          <span class="sr-only">(opens in a new window)</span></a> as they become available in a rapidly changing landscape. 
+          <a href="https://library.nyu.edu/services/remote/" target="_blank">all sourcing options
+            <span class="sr-only">(opens in a new window)</span>
+            <prm-icon external-link icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new">
+            </prm-icon>
+          </a> as they become available in a rapidly changing landscape. 
         </span>
       </div>
     </div>
