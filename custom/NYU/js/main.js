78d008ecfc853ec83e45f511e97779fb75b553ff
BAlter <barnaby.alter@nyu.edu>
2020-10-06 16:00:43 -0400

    Combine prototype component with existing custom-request component and hide requests when online links are available

--- custom/NYU/js/main.js
+++ custom/NYU/js/main.js
@@ -140,34 +140,42 @@ app
   })
   .component('prmLocationItemAfter', {
     template: /*html*/ `
-      <primo-explore-custom-requests
+      <primo-explore-custom-requests ng-hide="hideCustomRequests()"
         layout="row"
         layout-align="end center"
         layout-wrap
         flex-xs="100"
       ></primo-explore-custom-requests>
-      <hide-physical-items></hide-physical-items>
      `,
-    controller: ['$element', function ($element) {
+    controller: ['$element', '$window', '$scope', function ($element, $window, $scope) {
       const ctrl = this;
+
+      ctrl.$onInit = () => {
+        // Hide default request options if there are online links
+        const onlineLinks = angular.element($window.document).queryAll('#getit_link1_0 prm-view-online a');
+        const requestOptions = angular.element($window.document).queryAll('prm-location-items .md-list-item-text');
+
+        if (onlineLinks.length > 0) {
+          Array.from(requestOptions).forEach((requestOption) => {
+            requestOption.children().eq(2).css({ display: 'none' });
+          });
+        }
+      };
+
       ctrl.$postLink = () => {
         const $target = $element.parent().query('div.md-list-item-text');
         const $el = $element.query(`primo-explore-custom-requests`).detach();
         $target.append($el);
       };
-    }]
-  })
-  .component('hidePhysicalItems', {
-    template: `
-    <b>TEST</b>
-    `,
-    controller: ['$window', '$scope', '$injector', '$timeout', '$filter', function ($window, $scope, $injector, $timeout, $filter) {
-      const ctrl = this;
 
-      ctrl.$onInit = () => {
-        const $el = angular.element($window.document).queryAll('#getit_link1_0 prm-view-online a');
-        // console.log($el)
-        // $el && $el.children().eq(2).css({ display: 'none' });
+      $scope.hideCustomRequests = () => {
+        const onlineLinks = angular.element($window.document).queryAll('#getit_link1_0 prm-view-online a');
+
+        if (onlineLinks.length > 0) {
+          return true;
+        } else {
+          return false;
+        }
       };
     }]
   })
