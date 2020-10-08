2c0696f42fe1b7ba008c48961e28b88c8933fa8d
BAlter <barnaby.alter@nyu.edu>
2020-10-08 12:27:20 -0400

    Move custom request component stuff into external file and add to NYUSH and AD

--- custom/NYU/js/main.js
+++ custom/NYU/js/main.js
@@ -25,6 +25,7 @@ import customLoginConfig from 'Common/js/customLoginConfig';
 import topAlert from 'Common/js/topAlert';
 import topAlertConfig from 'Common/js/topAlertConfig';
 import physicalItemsAlert from 'Common/js/physicalItemsAlert';
+import customRequests from 'Common/js/customRequestComponent';
 import 'Common/js/sendToCourseReserves';
 
 // HTML as JS imports
@@ -138,47 +139,7 @@ app
   .component('prmBrowseSearchBarAfter', {
     template: /*html*/ `<search-bar-sub-menu></search-bar-sub-menu>`,
   })
-  .component('prmLocationItemAfter', {
-    template: /*html*/ `
-      <primo-explore-custom-requests ng-hide="hideCustomRequests()"
-        layout="row"
-        layout-align="end center"
-        layout-wrap
-        flex-xs="100"
-      ></primo-explore-custom-requests>
-     `,
-    controller: ['$element', '$window', '$scope', function ($element, $window, $scope) {
-      const ctrl = this;
-
-      ctrl.$onInit = () => {
-        // Hide default request options if there are online links
-        const onlineLinks = angular.element($window.document).queryAll('#getit_link1_0 prm-view-online a');
-        const requestOptions = angular.element($window.document).queryAll('prm-location-items .md-list-item-text');
-
-        if (onlineLinks.length > 0) {
-          Array.from(requestOptions).forEach((requestOption) => {
-            requestOption.children().eq(2).css({ display: 'none' });
-          });
-        }
-      };
-
-      ctrl.$postLink = () => {
-        const $target = $element.parent().query('div.md-list-item-text');
-        const $el = $element.query(`primo-explore-custom-requests`).detach();
-        $target.append($el);
-      };
-
-      $scope.hideCustomRequests = () => {
-        const onlineLinks = angular.element($window.document).queryAll('#getit_link1_0 prm-view-online a');
-
-        if (onlineLinks.length > 0) {
-          return true;
-        } else {
-          return false;
-        }
-      };
-    }]
-  })
+  .component('prmLocationItemAfter', customRequests)
   .component('prmLocationItemsAfter', {
     template: `${customRequestsRequestInformationTemplate}`
   })
