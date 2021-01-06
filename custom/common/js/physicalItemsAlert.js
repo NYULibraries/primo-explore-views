6c54a6be225466702d608c8f7134991f879d4930
BAlter <barnaby.alter@nyu.edu>
2021-01-05 22:16:53 -0500

    Update physical item alert text

--- custom/common/js/physicalItemsAlert.js
+++ custom/common/js/physicalItemsAlert.js
@@ -6,12 +6,14 @@ export default {
       <div class="bar large-bar layout-fill layout-padding layout-align-center-center layout-row error-bar">
         <span class="bar-text">
           <div>
-          Request Services will be paused during Winter Break starting the afternoon of December 21, 2020. Services and processing will resume the week of January 4th. For more information, visit 
-          <a href="https://nyulibraries.statuspage.io/incidents/ylqhtv3tm028" target="_blank">our status page
+          <p>Physical materials are available via scan, contact-less pick-up, or home delivery. See the explanation of request options below. There is no in-person access to NYU Libraries collections.
+
+          <p>For the latest information about using the libraries, visit our NYU Returns 
+          <a href="https://library.nyu.edu/nyu-returns/collections-access/" target="_blank">Library Collections Access
             <span class="sr-only">(opens in a new window)</span>
             <prm-icon external-link icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new">
             </prm-icon>
-          </a>.
+          </a> page.
           </div>
         </span>
       </div>
