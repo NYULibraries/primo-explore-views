52c685dcf488908182605897c61bc6031a8d9754
BAlter <barnaby.alter@nyu.edu>
2021-09-28 23:42:09 -0400

    Pull out TS jest config; build first dummy test for scaffold purposes

---
+++ custom/spec/customRequests.spec.js
@@ -0,0 +1,12 @@
+const angular = require('angular');
+require('angular-mocks');
+
+const customRequests = require('../common/js/customRequests.js');
+
+describe('customRequestIllComponentController', () => {
+  beforeEach( () => {});
+
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 /*
- * For a detailed explanation regarding each configuration property and type check, visit:
+ * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
 
-export default {
+module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -81,7 +81,9 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^Common/(.*)$':  '<rootDir>/custom/spec/dummy.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -137,7 +139,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
