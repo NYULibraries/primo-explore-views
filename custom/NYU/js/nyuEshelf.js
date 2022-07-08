5c80df9ff759a1f93c097bd7262605c84a458543
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2022-07-08 15:55:19 -0400

    attempted to install primo-explore-nyu-eshelf package from a GitHub branch;did not work

--- custom/NYU/js/nyuEshelf.js
+++ custom/NYU/js/nyuEshelf.js
@@ -1,15 +1,16 @@
 export default {
-  name: 'nyuEshelfConfig',
+  name: "nyuEshelfConfig",
   config: {
-    adding: 'Adding to Saved Items...',
-    addToEshelf: 'Add to Saved Items',
+    adding: "Adding to Saved Items...",
+    addToEshelf: "Add to Saved Items",
+    ariaLabel: "Toggle in Saved Items",
     defaultUrls: {
-      eshelfBaseUrl: 'https://eshelf-dev.library.nyu.edu',
+      eshelfBaseUrl: "https://eshelf-dev.library.nyu.edu",
     },
-    deleting: 'Removing from Saved Items...',
-    inEshelf: 'In Saved Items',
-    inGuestEshelf: 'In guest Saved Items',
-    myEshelfButtonClasses: 'button-over-dark search-bookmark-filter-item',
-    toolbar: 'Saved Items',
-  }
+    deleting: "Removing from Saved Items...",
+    inEshelf: "In Saved Items",
+    inGuestEshelf: "In guest Saved Items",
+    myEshelfButtonClasses: "button-over-dark search-bookmark-filter-item",
+    toolbar: "Saved Items",
+  },
 };
