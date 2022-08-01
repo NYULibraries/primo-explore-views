0900784bc83a40e4998459c00c89ea544e4a629b
David <da70@nyu.edu>
2022-08-01 13:17:42 -0400

    Example how to clear the "about://blank error": NYU/custom-no-search.results.cy.js

--- primo-explore-e2e-cypress/cypress/e2e/NYU/custom-no-search-results.cy.js
+++ primo-explore-e2e-cypress/cypress/e2e/NYU/custom-no-search-results.cy.js
@@ -1,11 +1,21 @@
+// This is a sample test file designed to demonstrate how to clear the errors seen
+// in this CircleCI test suite run:
+// https://app.circleci.com/pipelines/github/NYULibraries/primo-explore-views/1137/workflows/b27b6607-3c00-4f2a-956b-185f3a5bc0ab
+
+// When `experimentalSessionAndOrigin` is set to true, Cypress will set the page
+// to about:blank before every test.  For background:
+// - https://github.com/cypress-io/cypress/discussions/21186?sort=old
+// - https://docs.cypress.io/api/commands/session
+// - https://docs.cypress.io/api/commands/session#Why-are-all-my-Cypress-commands-failing-after-calling-cy-session
 describe('primo-explore-custom-no-search-results', function () {
   describe('when a search returns no results', () => {
     const searchTerm = `asdfhello;worldgoodbye;worldasdf`
 
-    before(() => {
+    // `beforeEach()` runs before each test in both the outer `describe()` and
+    // the inner `describe()`.
+    beforeEach(() => {
       cy.visit(`?query=any,contains,${searchTerm}&tab=all&search_scope=all&vid=NYU&offset=0`)
     })
-
 
     it('renders No records found', () => {
       cy.get(`[data-cy=no-search-results]`)
